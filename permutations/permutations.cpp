d4f717ec0f8796a3f1b12944315b8ee77d040925
Manshu <65810340+oraclesoul@users.noreply.github.com>
2021-06-01 12:05:29 +0530

    Update permutations.cpp

--- permutations/permutations.cpp
+++ permutations/permutations.cpp
@@ -1,13 +1,27 @@
+
 class Solution {
 public:
-    vector<vector<int>> permute(vector<int>& nums) {
-        
-        vector< vector< int > > ans;
-        sort(nums.begin(),nums.end());
-        ans.push_back(nums);
-        while(next_permutation(nums.begin(),nums.end()))
+    vector< vector< int > > ans;
+    void findAllPermu(vector< int > nums,int i)
+    {
+        if(i<0)
+        {
             ans.push_back(nums);
-        return ans;
-        
+            return;
+        }
+        for(int j=i;j>=0;j--)
+        {
+            swap(nums[i],nums[j]);
+            findAllPermu(nums,i-1);
+            swap(nums[i],nums[j]);
+        }
+        return;
     }
-};
+    vector<vector<int>> permute(vector<int>& nums) {
+        // to find in order
+        sort(nums.begin(),nums.end());
+        findAllPermu(nums,nums.size()-1);
+        return ans;       
+
+    }
+}; 
