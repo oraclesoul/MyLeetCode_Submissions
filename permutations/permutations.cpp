33c29d998f5220ac1eedcb420597a87f7a249353
Manshu <65810340+oraclesoul@users.noreply.github.com>
2021-06-01 11:48:00 +0530

    Time: 0 ms (100.00%), Space: 7.6 MB (84.45%) - LeetHub

--- permutations/permutations.cpp
+++ permutations/permutations.cpp
@@ -1,26 +1,13 @@
 class Solution {
 public:
-    vector< vector< int > > ans;
-    void findAllPermu(vector< int > nums,int i)
-    {
-        if(i<0)
-        {
-            ans.push_back(nums);
-            return;
-        }
-        for(int j=i;j>=0;j--)
-        {
-            swap(nums[i],nums[j]);
-            findAllPermu(nums,i-1);
-            swap(nums[i],nums[j]);
-        }
-        return;
-    }
     vector<vector<int>> permute(vector<int>& nums) {
-        // to find in order
+        
+        vector< vector< int > > ans;
         sort(nums.begin(),nums.end());
-        findAllPermu(nums,nums.size()-1);
-        return ans;       
+        ans.push_back(nums);
+        while(next_permutation(nums.begin(),nums.end()))
+            ans.push_back(nums);
+        return ans;
         
     }
 };
